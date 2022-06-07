0cb5857e06127a5ed18854bcca3670c94a2bcde4
chukwuemeka brian kelechi <brianchuks000@gmail.com>
2022-06-06 17:33:01 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include<unistd.h>
 /**
  * main - Entrypoint
- * Return; Always 0 (Success)
+ * Return; Always 1 (Success)
  */
-int main(void)
+int main()
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
